ac9e35965a9d34ac5e44dac4a8e0477a0aaa8069
Alberto Rota <alberto.rota.jimenez@gmail.com>
2016-10-28 19:09:29 +0200

    Steam release + bugfixes

--- scripts/weaponassembly/WA_weaponPainterMain.lua
+++ scripts/weaponassembly/WA_weaponPainterMain.lua
@@ -3,6 +3,7 @@ require "/scripts/weaponassembly/WA_paintManager.lua"
 
 function init()
   if storage.inventory == nil then storage.inventory = {} end
+  if storage.lastInventory == nil then storage.lastInventory = {} end
 end
 
 function die()
@@ -12,6 +13,7 @@ end
 function containerCallback(args)
   local slots = {}
   for i = 0, 4 do
+    storage.lastInventory[i + 1] = storage.inventory[i + 1]
     if inventorySlotChanged(i) then
       table.insert(slots, i)
     end
@@ -21,12 +23,25 @@ function containerCallback(args)
 end
 
 function containerSlotsChanged(slots)
-  if slots[1] == 0 or slots[1] == 1 or slots[1] == 2 or slots[1] == 3 then containerTakeItem(4) end
+  if slots[1] == 3 then
+      if storage.lastInventory[4] then
+        containerTakeItem(4)
+      else
+        world.spawnItem(containerSpitItem(4), entity.position())
+      end
+  end
+  if slots[1] == 0 or slots[1] == 1 or slots[1] == 2 then
+    if storage.lastInventory[4] and storage.lastInventory[5] then
+      containerTakeItem(4)
+    end
+  end
   if slots[1] == 4 then
-    containerTakeItem(0)
-    containerTakeItem(1)
-    containerTakeItem(2)
-    containerTakeItem(3)
+    if storage.lastInventory[4] and storage.lastInventory[5] then
+      containerTakeItem(0)
+      containerTakeItem(1)
+      containerTakeItem(2)
+      containerTakeItem(3)
+    end
   end
 
   local weapon = paintWeapon()
@@ -44,6 +59,11 @@ end
 function containerTakeItem(slot)
   world.containerTakeNumItemsAt(entity.id(), slot, 1)
   storage.inventory[slot+1] = world.containerItemAt(entity.id(), slot)
+end
+
+function containerSpitItem(slot)
+  storage.inventory[slot+1] = nil
+  return world.containerTakeAt(entity.id(), slot)
 end
 
 function containerPutItem(item, slot)
